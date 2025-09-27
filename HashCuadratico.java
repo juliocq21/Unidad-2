d2a1eb0f97d41089cf0c529463fb374fde5100ff
juliocq21 <U24222988@utp.edu.pe>
2025-09-26 23:13:53 -0500

    Add files via upload

--- HashCuadratico.java
+++ HashCuadratico.java
@@ -9,5 +9,34 @@ package unidad2;
  * @author user
  */
 public class HashCuadratico {
-    
+    private String[] tabla;
+    private int tamaño;
+
+    public HashCuadratico(int tamaño) {
+        this.tamaño = tamaño;
+        this.tabla = new String[tamaño];
+    }
+
+    private int funcionHash(int clave) {
+        return clave % tamaño;
+    }
+
+    // Inserción con sondeo cuadrático
+    public void insertar(int clave, String valor) {
+        int indice = funcionHash(clave);
+        int i = 1;
+        while (tabla[indice] != null) {
+            indice = (indice + i * i) % tamaño;
+            i++;
+        }
+        tabla[indice] = valor;
+        System.out.println("Insertado " + valor + " en indice " + indice);
+    }
+
+    public void mostrar() {
+        System.out.println("\nTabla (Sondeo Cuadratico):");
+        for (int i = 0; i < tamaño; i++) {
+            System.out.println("[" + i + "] => " + tabla[i]);
+        }
+    }
 }
